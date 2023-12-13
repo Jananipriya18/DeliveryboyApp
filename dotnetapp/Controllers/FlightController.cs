0749e2fd6e46a6018ca746bbe49c03067810381a
Iamneo <techadmin@iamneo.ai>
2023-12-13 12:06:21 +0000

    Auto-save-commit-12/13/2023--5:36:15-PM

--- dotnetapp/Controllers/FlightController.cs
+++ dotnetapp/Controllers/FlightController.cs
@@ -1,49 +1,38 @@
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
-using dotnetapp.Models; // Ensure to include your Flight model namespace
+using dotnetapp.Models;
 
 namespace dotnetapp.Controllers
 {
     public class FlightController : Controller
+{
+    private static readonly List<Flight> _flightList = new List<Flight>
     {
-        private readonly List<Flight> _flightList; // Collection to store flight information
+        new Flight("Mumbai", "01h 45m", "Dubai", "₹ 15903", "flydubai", "2137"),
+        new Flight("New Delhi", "03h 30m", "Dubai", "₹ 17070", "Emirates", "511")
+    };
 
-        public FlightController()
-        {
-            // Initialize an empty list for flights
-            _flightList = new List<Flight>();
-        }
-
-        // Action to display the flight creation form
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // Action to handle form submission and create a new flight
-        [HttpPost]
-        public IActionResult Create(Flight newFlight)
-        {
-            Console.WriteLine(newFlight);
-             if (ModelState.IsValid)
-            // {
-                // Add the submitted flight to the flight list
-                _flightList.Add(newFlight);
-
-                // Redirect to the index or any other appropriate action
-                return RedirectToAction("Index");
-             }
-
-            // If the model state is not valid, return back to the form view
-            return View(newFlight);
-        }
-
-        // Action to display all flights
-        public IActionResult Index()
-        {
-            return View(_flightList);
-        }
-
-        // Other actions for handling flights...
+    public IActionResult Create()
+    {
+        return View();
     }
+
+    [HttpPost]
+    public IActionResult Create(Flight newFlight)
+    {
+        if (ModelState.IsValid)
+        {
+            _flightList.Add(newFlight);
+            return RedirectToAction("Index");
+        }
+
+        return View(newFlight);
+    }
+
+    public IActionResult Index()
+    {
+        return View(_flightList);
+    }
+}
+
 }

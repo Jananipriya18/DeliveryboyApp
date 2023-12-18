a33f67bc4c9006bb1022d9dd014301f3537a6de8
Iamneo <techadmin@iamneo.ai>
2023-12-18 11:30:42 +0000

    Auto-save-commit-12/18/2023--5:00:40-PM

--- crudapp/Models/Furniture.cs
+++ crudapp/Models/Furniture.cs
@@ -9,3 +9,10 @@ using System;
      public string material { get; set; }
      public int cost { get; set; }
  }
+
+ INSERT INTO Flights (id, product, description, material, cost)
+VALUES 
+    (5, 'Chair', 'Comfortable chair for home or office use', 'Wood', 80),
+    (6, 'Sofa', 'Large sofa for living room', 'Fabric', 300),
+    (7, 'Table', 'Modern coffee table', 'Glass', 150),
+    (8, 'Bookshelf', 'Wooden bookshelf with multiple shelves', 'Wood', 200);

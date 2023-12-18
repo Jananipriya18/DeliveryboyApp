c419b951750f5f04651e74c2da4c62cdbb743349
Iamneo <techadmin@iamneo.ai>
2023-12-18 10:45:41 +0000

    Auto-save-commit-12/18/2023--4:15:40-PM

--- crudapp/Models/Furniture.cs
+++ crudapp/Models/Furniture.cs
@@ -1,4 +1,11 @@
 // tableCreation
 
 using System;
-public class Furniture{public int id,public }
+ public class Furniture
+ {
+     public int id { get; set; } // Unique identifier for each flight
+     public string product { get; set; }
+     public string description { get; set; }
+     public string material { get; set; }
+     public int cost { get; set; }
+ }

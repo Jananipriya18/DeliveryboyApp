9e8fd5fca8999827c5e354a72a03068e15cb1618
Iamneo <techadmin@iamneo.ai>
2023-12-19 11:03:55 +0000

    Auto-save-commit-12/19/2023--4:33:53-PM

---
+++ bikeapp/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+namespace bikeapp.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<B
+    }
+}
---
+++ bikeapp/Models/Bike.cs
@@ -0,0 +1,12 @@
+namespace bikeapp.Models
+public class Bike
+{
+    public int BikeId { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public double Price { get; set; }
+    public string Color { get; set; }
+    
+    
+}

4eb28d7952c91786346e5463ee438f7a029577d6
Iamneo <techadmin@iamneo.ai>
2023-12-22 14:28:45 +0000

    Auto-save-commit-12/22/2023--7:58:42-PM

--- Deliveryboy/Models/ApplicationDbContext.cs
+++ Deliveryboy/Models/ApplicationDbContext.cs
@@ -1,13 +1,12 @@
-using Microsoft.EntityFrameworkCore;
-
-namespace bikeapp.Models
+namespace Deliveryboy.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
         public DbSet<Order> Order { get; set; }
         public DbSet<Delivery> Delivery { get; set; }
     }
